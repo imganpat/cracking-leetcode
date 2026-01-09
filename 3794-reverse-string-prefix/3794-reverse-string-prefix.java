0e758a7470e3357d15d728471b255dd288acd652
Ganpat <coolganpat41@gmail.com>
2026-01-09 19:14:04 +0530

    Time: 2 ms (53.44%), Space: 44.5 MB (88.52%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.java
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -1,7 +1,10 @@
 class Solution {
     public String reversePrefix(String s, int k) {
+        if (k == 1) {
+            return s;
+        }
         StringBuilder rev = new StringBuilder(s);
-        for(int left = 0, right = k -1; left < right; left++, right--){
+        for (int left = 0, right = k - 1; left < right; left++, right--) {
             char c = rev.charAt(left);
             rev.setCharAt(left, rev.charAt(right));
             rev.setCharAt(right, c);
