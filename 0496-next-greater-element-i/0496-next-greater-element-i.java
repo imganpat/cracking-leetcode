f32aba56f6897ea1e0c83041f01053047d8fde02
Ganpat <coolganpat41@gmail.com>
2026-01-27 21:50:01 +0530

    Time: 4 ms (73.5%), Space: 45.3 MB (42.55%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,35 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> stack = new Stack<>();
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> res = new ArrayList<>();
 
-         // store index of each number in nums2
-        for (int i = 0; i < nums2.length; i++) {
-            map.put(nums2[i], i);
-        }
-
-        for (int i = 0; i < nums1.length; i++) {
-            int nextGreater = -1;
-            
-            if (map.containsKey(nums1[i])) {
-                for (int j = map.get(nums1[i]) + 1; j < nums2.length; j++) {
-                    if (nums1[i] < nums2[j]) {
-                        nextGreater = nums2[j];
-                        break;
-                    }
-                }
+        for (int num : nums2) {
+            while(!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
             }
-
-            // Always add one result per nums1 element
-            res.add(nextGreater);
+            stack.push(num);
         }
 
-        int[] result = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) {
-            result[i] = res.get(i);
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
         }
 
-        return result;
+        int n = nums1.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = map.get(nums1[i]);
+        }
 
+        return res;
     }
 }
