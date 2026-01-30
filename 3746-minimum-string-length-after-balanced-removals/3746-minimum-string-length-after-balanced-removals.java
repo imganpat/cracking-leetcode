313710893b4d9ac75cde449ef0b231d31bcecc6c
Ganpat <coolganpat41@gmail.com>
2026-01-30 20:06:48 +0530

    Time: 32 ms (6.41%), Space: 48 MB (6.2%) - LeetHub

---
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minLengthAfterRemovals(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() != c){
+                stack.pop();
+            }else{
+                stack.push(c);
+            }
+        }
+
+        return stack.size();
+    }
+}
