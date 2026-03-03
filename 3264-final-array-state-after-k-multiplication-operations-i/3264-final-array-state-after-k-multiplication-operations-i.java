6ff1c1838306ac4f4b67f8678d50b10449fbb1f3
Ganpat <coolganpat41@gmail.com>
2026-03-03 21:21:49 +0530

    Time: 4 ms (20.16%), Space: 46.7 MB (90.7%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -1,23 +1,39 @@
+import java.util.*;
+
 class Solution {
-    public int getMinIndex(int[] nums) {
-        int min = 0;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[min] > nums[i]) {
-                min = i;
-            }
-        }
-
-        return min;
-    }
 
     public int[] getFinalState(int[] nums, int k, int multiplier) {
+
+        // Min heap storing {value, index}
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> {
+                    if (a[0] == b[0]) {
+                        return a[1] - b[1]; // tie-break by index
+                    }
+                    return a[0] - b[0];
+                });
+
+        // Add elements with index
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(new int[] { nums[i], i });
+        }
+
+        // Perform k operations
         while (k > 0) {
-            int i = getMinIndex(nums);
-            nums[i] *= multiplier;
+            int[] smallest = pq.poll();
+            smallest[0] *= multiplier; // multiply value
+            pq.add(smallest); // push back
             k--;
         }
 
-        return nums;
+        // Build final result array
+        int[] result = new int[nums.length];
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            result[curr[1]] = curr[0];
+        }
+
+        return result;
     }
 }
