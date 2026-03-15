0d2dc6391727f951223f158c091b6b3650aea647
Ganpat <coolganpat41@gmail.com>
2026-03-15 15:49:49 +0530

    Time: 3 ms (76.53%), Space: 42.9 MB (89.67%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,20 +1,22 @@
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character, Integer> freq = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        int[] freq = new int[26];
+        for(char c: s.toCharArray()){
+            freq[c - 'a']++;
         }
 
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (Map.Entry<Character, Integer> e : freq.entrySet()) {
-            maxHeap.offer(new int[] { e.getValue(), e.getKey() });
+        for(int i = 0; i < 26; i++){
+            if (freq[i] > 0) {
+                maxHeap.offer(new int[]{freq[i], i});
+            }
         }
 
         StringBuilder res = new StringBuilder();
         int[] prev = null;
         while (!maxHeap.isEmpty()) {
             int[] curr = maxHeap.poll();
-            res.append((char) curr[1]);
+            res.append((char) (curr[1] + 'a'));
             curr[0]--;
 
             if (prev != null && prev[0] > 0) {
