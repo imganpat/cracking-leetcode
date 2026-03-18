24881ae87ae347c7bfb8e19bf83749fa38c1ca8d
Ganpat <coolganpat41@gmail.com>
2026-03-18 21:31:53 +0530

    Time: 30 ms (63.3%), Space: 47.9 MB (71.18%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.java
@@ -1,25 +1,25 @@
 class Solution {
     public String repeatLimitedString(String s, int repeatLimit) {
-        Map<Character, Integer> freq = new HashMap<>();
+        int[] freq = new int[26];
         for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            freq[c - 'a']++;
         }
 
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-
-        for (Map.Entry<Character, Integer> e : freq.entrySet()) {
-            maxHeap.offer(new int[] { e.getKey(), e.getValue() });
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                maxHeap.offer(new int[] { i + 'a', freq[i] });
+            }
         }
 
         StringBuilder res = new StringBuilder();
-
         while (!maxHeap.isEmpty()) {
+
             int[] top = maxHeap.poll();
             char c = (char) top[0];
             int f = top[1];
 
             int use = Math.min(f, repeatLimit);
-
             for (int i = 0; i < use; i++) {
                 res.append(c);
             }
@@ -39,7 +39,7 @@ class Solution {
                     maxHeap.offer(sTop);
                 }
 
-                maxHeap.offer(new int[]{c, f});
+                maxHeap.offer(new int[] { c, f });
             }
         }
 
