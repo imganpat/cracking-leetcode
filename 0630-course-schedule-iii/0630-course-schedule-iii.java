b99e44a971eef618ffc64ba49ac4840de9567327
Ganpat <coolganpat41@gmail.com>
2026-03-20 15:03:56 +0530

    Time: 47 ms (33.22%), Space: 56.4 MB (67.86%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.java
+++ 0630-course-schedule-iii/0630-course-schedule-iii.java
@@ -1,26 +1,23 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        for(int[] course: courses){
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        for (int[] course : courses) {
             minHeap.offer(course);
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b-a);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
 
-        int courseTaken = 0;
         int days = 0;
-        while(!minHeap.isEmpty()){
+        while (!minHeap.isEmpty()) {
             int[] top = minHeap.poll();
             days += top[0];
             maxHeap.offer(top[0]);
-            courseTaken++;
-            
-            if(days > top[1]){
+
+            if (days > top[1]) {
                 days -= maxHeap.poll();
-                courseTaken--;
             }
         }
 
-        return courseTaken;
+        return maxHeap.size();
     }
 }
