ef7f716c32de9eaac053145707d038df729827d5
Ganpat <coolganpat41@gmail.com>
2026-04-05 14:09:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -8,15 +8,19 @@ class Solution {
             return null;
         }
 
-        String res = backtrack(nums, new StringBuilder(curr + "0"), i+1);
+        curr.append('0');
+        String res = backtrack(nums, curr, i+1);
         if(res != null){
             return res;
         }
+        curr.deleteCharAt(curr.length() - 1);
         
-        res = backtrack(nums, new StringBuilder(curr + "1"), i+1);
+        curr.append('1');
+        res = backtrack(nums, curr, i+1);
         if(res != null){
             return res;
         }
+        curr.deleteCharAt(curr.length() - 1);
 
         return null;
 
