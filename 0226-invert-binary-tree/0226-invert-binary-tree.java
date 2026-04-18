28672be593a298555c554b538be279b009376b8c
Ganpat <coolganpat41@gmail.com>
2026-04-18 19:54:28 +0530

    Time: 0 ms (100%), Space: 42.7 MB (92.42%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -14,31 +14,46 @@
  * }
  */
 class Solution {
+    public void invert(TreeNode node){
+        if(node == null){
+            return;
+        }
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        invert(node.left);
+        invert(node.right);
+    }
+
     public TreeNode invertTree(TreeNode root) {
-        if(root == null){
-            return root;
-        }
-        
-        Deque<TreeNode> queue = new ArrayDeque<>();
-        
-        queue.offer(root);
-        
-        while(!queue.isEmpty()){
-            TreeNode curr = queue.poll();
-            
-            TreeNode temp = curr.left;
-            curr.left = curr.right;
-            curr.right = temp;
-
-            if(curr.left != null){
-                queue.offer(curr.left);
-            }
-
-            if(curr.right != null){
-                queue.offer(curr.right);
-            } 
-        }
+        invert(root);
         
         return root;
+        // if(root == null){
+        //     return root;
+        // }
+        
+        // Deque<TreeNode> queue = new ArrayDeque<>();
+        
+        // queue.offer(root);
+        
+        // while(!queue.isEmpty()){
+        //     TreeNode curr = queue.poll();
+            
+        //     TreeNode temp = curr.left;
+        //     curr.left = curr.right;
+        //     curr.right = temp;
+
+        //     if(curr.left != null){
+        //         queue.offer(curr.left);
+        //     }
+
+        //     if(curr.right != null){
+        //         queue.offer(curr.right);
+        //     } 
+        // }
+        
+        // return root;
     }
 }
