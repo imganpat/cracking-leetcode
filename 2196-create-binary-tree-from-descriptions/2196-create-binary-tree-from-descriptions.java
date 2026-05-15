257d476cff06c572f57d39a8fd403f61b6fefaba
Ganpat <coolganpat41@gmail.com>
2026-05-15 19:48:18 +0530

    Time: 89 ms (6.65%), Space: 87.2 MB (18.2%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, TreeNode> map = new HashMap<>();
+    Map<Integer, Boolean> hasParent = new HashMap<>();
+
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        for (int[] desc: descriptions) {
+            int parentVal = desc[0];
+            int childVal = desc[1];
+            int isLeft = desc[2];
+            TreeNode parentNode = null;
+            TreeNode childNode = null;
+
+            if (map.containsKey(parentVal)) 
+                parentNode = map.get(parentVal);
+            else
+                parentNode = new TreeNode(parentVal);
+
+            if (map.containsKey(childVal))
+                childNode = map.get(childVal);
+            else
+                childNode = new TreeNode(childVal);
+
+            if (isLeft == 1) parentNode.left = childNode;
+            else parentNode.right = childNode;
+
+            hasParent.put(childVal, true);
+            map.put(parentVal, parentNode);
+            map.put(childVal, childNode);
+        }
+
+        TreeNode root = null;
+        for (Map.Entry<Integer, TreeNode> e: map.entrySet()){
+            int nodeVal = e.getKey();
+            TreeNode node = e.getValue();
+
+            if (!hasParent.getOrDefault(nodeVal, false))
+                root = node;
+        }
+        return root;
+    }
+}
