a2f14b7897fe731a1b42467ff5fbd25d42bb6649
Ganpat <coolganpat41@gmail.com>
2026-05-18 11:07:21 +0530

    Time: 2 ms (69.53%), Space: 46.8 MB (97.03%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -1,30 +1,30 @@
 class Solution {
     List<String> res = new ArrayList<>();
+
     public void backtrack(StringBuilder curr, int i) {
         if (i < 0) {
-            res.add(curr.toString()); 
+            res.add(curr.toString());
             return;
         }
 
         if (Character.isDigit(curr.charAt(i))) {
-            backtrack(curr, i-1);
-        }
-        else {
+            backtrack(curr, i - 1);
+        } else {
             char original = curr.charAt(i);
 
             curr.setCharAt(i, Character.toLowerCase(original));
-            backtrack(curr, i-1);
-            
+            backtrack(curr, i - 1);
+
             curr.setCharAt(i, Character.toUpperCase(original));
-            backtrack(curr, i-1);
-            
-            curr.setCharAt(i, original);
+            backtrack(curr, i - 1);
+
+            // curr.setCharAt(i, original);
         }
 
     }
 
     public List<String> letterCasePermutation(String s) {
-        backtrack(new StringBuilder(s), s.length()-1);
+        backtrack(new StringBuilder(s), s.length() - 1);
         return res;
     }
 }
