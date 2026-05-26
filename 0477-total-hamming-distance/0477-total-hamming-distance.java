381f883d55d3f984078e8bdf97bcd433ca152897
Ganpat <coolganpat41@gmail.com>
2026-05-26 12:08:41 +0530

    Time: 1790 ms (8.7%), Space: 48.1 MB (48.89%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int res = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                res += Integer.bitCount(nums[i] ^ nums[j]);
+            }
+        }
+
+        return res;
+    }
+}
