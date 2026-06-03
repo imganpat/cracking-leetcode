56ad1f41c392bece7a0bb752ac48191905d67d24
Ganpat <coolganpat41@gmail.com>
2026-06-03 16:47:37 +0530

    Time: 1 ms (99.9%), Space: 43.5 MB (46.17%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.java
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.java
@@ -1,10 +1,10 @@
 class Solution {
     public int countKeyChanges(String s) {
+        int n = s.length();
         String l = s.toLowerCase();
-        
         int count = 0;
 
-        for (int i = 1; i < s.length(); i++) {
+        for (int i = 1; i < n; i++) {
             if (l.charAt(i - 1) != l.charAt(i)) {
                 count++;
             }
