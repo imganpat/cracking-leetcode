3ff00d4492a15a40a29f685bfffb75c9adb77fb7
Ganpat <coolganpat41@gmail.com>
2026-06-12 21:39:49 +0530

    Time: 6 ms (100%), Space: 81.2 MB (76.39%) - LeetHub

--- 2575-find-the-divisibility-array-of-a-string/2575-find-the-divisibility-array-of-a-string.java
+++ 2575-find-the-divisibility-array-of-a-string/2575-find-the-divisibility-array-of-a-string.java
@@ -5,8 +5,9 @@ class Solution {
 
         long rem = 0;
         for (int i = 0; i < n; i++) {
-            rem = (rem * 10 + Character.getNumericValue(word.charAt(i))) % m;
-
+            // rem = (rem * 10 + Character.getNumericValue(word.charAt(i))) % m;
+            rem = (rem * 10 + word.charAt(i) - '0' ) % m;
+            
             if (rem == 0) {
                 res[i] = 1;
             }
