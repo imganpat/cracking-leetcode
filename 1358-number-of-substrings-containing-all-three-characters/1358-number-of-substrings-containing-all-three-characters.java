d4107ec7c1465154fd66c3aaefb769441986c4a7
Ganpat <coolganpat41@gmail.com>
2026-06-30 20:46:18 +0530

    Time: 56 ms (5.05%), Space: 47.1 MB (10.46%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -9,8 +9,8 @@ class Solution {
 
         while (end < n) {
             char rightChar = s.charAt(end);
-            // freq.put(rightChar, freq.getOrDefault(rightChar, 0) + 1);
-            freq.merge(rightChar, 1, Integer::sum);
+            freq.put(rightChar, freq.getOrDefault(rightChar, 0) + 1);
+            // freq.merge(rightChar, 1, Integer::sum);
             
             while (freq.containsKey('a') &&
                     freq.containsKey('b') &&
@@ -19,8 +19,8 @@ class Solution {
                 count += n - end;
 
                 char leftChar = s.charAt(start);
-                // freq.put(leftChar, freq.get(leftChar) - 1);
-                freq.merge(leftChar, -1, Integer::sum);
+                freq.put(leftChar, freq.get(leftChar) - 1);
+                // freq.merge(leftChar, -1, Integer::sum);
 
                 if (freq.get(leftChar) == 0) {
                     freq.remove(leftChar);
