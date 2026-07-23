1f832ecbfeb85219aba8676c1126523339251922
Ganpat <coolganpat41@gmail.com>
2026-07-23 17:40:01 +0530

    Time: 1 ms (98.91%), Space: 47.5 MB (16.66%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[][] DIRECTIONS = { {1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    int[][] DIRECTIONS = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
     boolean[][] visited;
 
     public int countBattleships(char[][] board) {
@@ -21,22 +21,24 @@ class Solution {
     }
 
     private void dfs(char[][] board, int row, int col) {
+
+        if (!isValid(board.length, board[0].length, row, col))
+            return;
+
+        if (visited[row][col])
+            return;
+
+        if (board[row][col] == '.')
+            return;
+
         visited[row][col] = true;
 
-        for (int[] direction: DIRECTIONS) {
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-
-            if (isValid(board.length, board[0].length, newRow, newCol)
-                    && board[newRow][newCol] == 'X'
-                    && !visited[newRow][newCol]) {
-                dfs(board, newRow, newCol);
-            }
+        for (int[] direction : DIRECTIONS) {
+            dfs(board, row + direction[0], col + direction[1]);
         }
-        
     }
 
     private boolean isValid(int n, int m, int row, int col) {
-        return row >= 0 && row < n && col >=0 && col < m;
+        return row >= 0 && row < n && col >= 0 && col < m;
     }
 }
