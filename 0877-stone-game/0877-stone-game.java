5b2f8281b5c2bf5aef8644c30d3449850c9b5b76
Ganpat <coolganpat41@gmail.com>
2026-08-02 20:54:29 +0530

    Time: 13 ms (5.73%), Space: 49.9 MB (5.29%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,29 @@
 class Solution {
+    Integer[][] memo;
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        memo = new Integer[n][n];
+        int diff = helper (piles, 0, n - 1);
+        return diff > 0;
+    }
+
+    private int helper(int[] piles, int l, int r) {
+        if (l > r) {
+            return 0;
+        }
+
+        if (l == r) {
+            return piles[l];
+        }
+
+        if (memo[l][r] != null) {
+            return memo[l][r];
+        }
+
+        int left = piles[l] - helper(piles, l + 1, r);
+        int right = piles[r] - helper(piles, l, r - 1);
+
+        memo[l][r] = Math.max(left, right);
+        return memo[l][r];
     }
 }
