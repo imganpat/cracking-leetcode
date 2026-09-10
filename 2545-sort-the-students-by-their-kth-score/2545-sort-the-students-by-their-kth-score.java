e4976121bb4fcc976f01f68c543f218a0996330a
Ganpat <theganpatsmail@gmail.com>
2026-09-10 21:02:54 +0530

    Time: 2 ms (98.12%), Space: 56 MB (63.65%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -1,19 +1,8 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) {
-        for (int  i = 0; i < score.length; i++) {
-            for (int j = 1; j < score.length - i; j++) {
-                if (score[j - 1][k] < score[j][k]) {
-                    swap(score, j - 1, j);
-                }
-            }
-        }
-
+        
+        Arrays.sort(score, (a, b) -> b[k] - a[k]);
+        
         return score;
-    }
-
-    private void swap(int[][] score,  int i, int j) {
-        int[] temp = score[i];
-        score[i] = score[j];
-        score[j] = temp;
     }
 }
